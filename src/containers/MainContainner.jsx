f2d3cbec1852e94835bb57173eecb40adf9e05b2
福山帆士 <fukuyamahoshi@fukuyamahoshinoMacBook-Pro.local>
2021-03-26 00:50:48 +0900

    edit method add.

--- src/containers/MainContainner.jsx
+++ src/containers/MainContainner.jsx
@@ -10,12 +10,16 @@ function MainContainer() {
 
     const createUrl = 'http://localhost:3001/pont/create';
     const deleteUrl = 'http://localhost:3001/pont/delete';
+    const editUrl = 'http://localhost:3001/pont/edit';
 
     const [nameValue, setNamevalue] = useState('');
     const [ageValue, setAgevalue] = useState('');
     const [sexValue, setSexvalue] = useState('');
 
     const [user, setUser] = useState([]);
+
+    const [editFlg, setEditFlg] = useState(false);
+    const [editTargetId, setEditTargetId] = useState('');
 
     const handleNameChange = (e) => {
         setNamevalue(e.target.value);
@@ -30,7 +34,7 @@ function MainContainer() {
     }
 
     const handleButtonSubmit = (e) => {
-
+        if (editFlg == false) {
         const myUid = getUniqueStr()
 
         axios.post(createUrl, {
@@ -53,6 +57,33 @@ function MainContainer() {
         .catch((error) => {
             console.log(error);
         })
+       
+    }else {
+           axios.post(editUrl, {
+               name: nameValue,
+               age: ageValue,
+               sex: sexValue,
+               uuid: editTargetId
+           })
+           .then((response) => {
+               var targetUserAr = user.map(function(i) {
+                   if (i.uuid === editTargetId) {
+                       i.name = nameValue;
+                       i.age = ageValue;
+                       i.sex = sexValue;
+                       return i;
+                   }else {
+                       return i;
+                   }
+               });
+               setEditFlg(false);
+               setUser(targetUserAr);
+           })
+           .catch((error) => {
+               console.log(error);
+               console.log('失敗');
+           })
+       }
     }
 
     const deleteUser = (e) => {
@@ -73,6 +104,20 @@ function MainContainer() {
         })
     }
 
+    const editUser = (e) => {
+        let targetUid = e.currentTarget.getAttribute('data-num');
+        var targetUserArray = user.filter(function(i) {
+            return i.uuid === targetUid;
+        });
+
+        let targetUser = targetUserArray[0];
+        setNamevalue(targetUser.name);
+        setAgevalue(targetUser.age);
+        setSexvalue(targetUser.sex);
+        setEditFlg(true);
+        setEditTargetId(targetUid);
+    }
+
     function getUniqueStr() {
         var strong = 1000;
         return new Date().getTime().toString(16) + Math.floor(strong * Math.random()).toString(16);
@@ -90,7 +135,7 @@ function MainContainer() {
             </div>
             {
                 user.map((value, index) => (
-                    <SubContainer nameValue={value.name} ageValue={value.age} sexValue={value.sex} uuidValue={value.uuid} deleteClick={deleteUser}/>
+                    <SubContainer nameValue={value.name} ageValue={value.age} sexValue={value.sex} uuidValue={value.uuid} deleteClick={deleteUser} editClick={editUser}/>
                 ))
             }
         </div>
